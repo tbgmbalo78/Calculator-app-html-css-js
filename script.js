061f783151b5118a92a7af261b7710ab720f859f
tbgmbalo78 <73177226+tbgmbalo78@users.noreply.github.com>
2025-06-16 14:23:08 +0200

    event handler of the buttons

---
+++ script.js
@@ -0,0 +1,14 @@
+//Access DOM elements of the calculator
+const inputBox = document.getElementById("input");
+const expressionDiv = document.getElementById("expression");
+const resultDiv = document.getElementById("result");
+
+//Define expression and result variable
+let expression = "";
+let result = "";
+
+//Define event handler for button clicks
+function buttonClicks(event) {
+  //Get values from clicked button
+  const target = event.target;
+}
